8d4a58d0599c86b211ca411128530d5c1d526fc0
LokiTheMango <mirkomueller97@live.de>
2018-05-24 10:15:01 +0200

    Finish pong

--- main.asm
+++ main.asm
@@ -6,6 +6,8 @@ ball_ry EQU 36h
 balken_1 EQU 33h
 balken_2 EQU 34h
 
+ende EQU 37h
+
 cseg at 0h
 ajmp init
 cseg at 100h
@@ -16,10 +18,15 @@ reti
 
 org 20h
 init:
+call clear_screen
+mov ende, #0h
 mov ball_x, #3h
 mov ball_y, #3h
+mov balken_1, #0h
+mov balken_2, #0h
+mov ball_rx, #0h
+mov ball_ry, #0h
 call move_balken
-
 mov ie, #10010010b
 mov tmod, #00000010b
 mov r7, #00h
@@ -30,12 +37,20 @@ setb p0.0
 setb tr0
 
 run:
-;call draw_our_shit
+mov a, ende
+cjne a, #0h, init
 jmp run
 
 timer:
 call draw_our_shit
+
+mov a, ende
+cjne a, #0h, end_timer
+
 call do_logic
+ret
+end_timer:
+clr tr0
 ret
 
 draw_our_shit:
@@ -73,8 +88,8 @@ paint_balken:
 	ret
 
 move_ball:
-	call move_ball_x
 	call move_ball_y
+	call move_ball_x
 	ret
 
 move_ball_y:
@@ -107,15 +122,27 @@ move_ball_x:
 
 	ball_richtung_x:
 	djnz ball_x, continue_check_dec
-	jmp init
+	mov ende, #1h
+	jmp ball_ende_x
+	
 	continue_check_dec:
 	mov a, ball_x
 	cjne a, #1h, ball_ende_x
 	mov a, ball_y
-	;mov r2, balken_1
-	;cjne a, r2, switch_dir
-	cjne a, balken_1, switch_dir
-	cjne a, balken_1, switch_dir
+	mov b, balken_1
+
+	cjne a, b, next1
+	jmp switch_dir
+
+	next1:
+	inc b
+	cjne a, b, next2
+	jmp switch_dir
+
+	next2:
+	inc b
+	cjne a, b, ball_ende_x
+
 	switch_dir:
 	mov ball_rx, #0h
 	ball_ende_x:
